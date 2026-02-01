80ef5963a7cdc95880be50c314eee75a1cefd940
Baks <trawallybakary7@gmail.com>
2026-02-01 22:30:17 +0000

    Fix mongoose dependency

--- backendServer/config/dbConnection.js
+++ backendServer/config/dbConnection.js
@@ -3,20 +3,19 @@ const mongoose = require("mongoose");
 
 // personnel db
 const connectPersonnelDB = mongoose.createConnection(
-  process.env.PERSONNER_CONNECTION,
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  process.env.PERSONNER_CONNECTION
+  // { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 // login db
 const connectLoginDB = mongoose.createConnection(
-  process.env.LOGIN_CONNECTION,
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  process.env.LOGIN_CONNECTION
 );
 
 // leave db
 const connectLeaveDB = mongoose.createConnection(
-  process.env.LEAVE_CONNECTION,
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  process.env.LEAVE_CONNECTION
+  
 );
 
 
